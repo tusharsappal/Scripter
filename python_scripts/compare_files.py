e39e4312ed2d1880244b13317213d5e5d47623ce
tusharsappal <sappal.tushar@gmail.com>
2013-11-09 16:41:23 +0530

    adding the file to compare two text files

---
+++ python_scripts/compare_files.py
@@ -0,0 +1,38 @@
+## this script compares  the two text files
+## first line of defence is to match the number of line in the file
+
+def compare_two_files(str_1,str_2):
+    print "Comparing starts"
+    print"First fetching the number of line"
+    with open(str_1) as fin :
+        lines = sum (1 for line in fin)
+    with open(str_2) as fin_2:
+        lines_2 = sum(1 for line in fin_2)
+
+    if(lines== lines_2):
+        print "First Line of defence is passed"
+        ins = open (str_1,"r")
+        array = []
+        for line_1 in ins:
+            array.append(line_1)
+        ins.close()
+        ins_2 = open (str_2,"r")
+        array_2=[]
+        for line_2 in ins_2:
+            array_2.append(line_2)
+        ins_2.close()
+        if (array==array_2):
+            print "In the Second Line of Defence the file has passed"
+        else :
+            print "In the second line of defence the file has failed"
+        
+        
+    else :
+        print "First line of defence is not pased"
+
+
+
+## replace the first parameter with the first file path and the second paramter with the second file path 
+compare_two_files("path to the first file ","path to the second file")
+        
+    
