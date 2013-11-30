f49546b10d250358c9bcc355d560541542cd579b
Tushar Sappal <sappal.tushar@gmail.com>
2013-11-30 14:58:22 +0530

    Adding the check for the email address that it should start with alphanumeric data  types and end with alphabets.

--- python_scripts/fetch_email_address.py
+++ python_scripts/fetch_email_address.py
@@ -2,18 +2,20 @@
 __author__ = 'tusharsappal'
 
 
-## This script fetches the email addresses from the string provided , the script only fetches the email addresses of the format anynumberofnonwhitespacecharacters_@_anynumberofnonwhitespacecharcters
+## This script fetches the email addresses from the string provided , the script only fetches the email addresses of the format alphanumeric@alphabets
 
 import re
 def fetch_email_address(str):
-    fetcher=re.findall('\S+@\S+',str)
-    print fetcher
+    fetcher=re.findall('[a-zA-Z0-9]\S+@\S+[a-zA-Z]]',str)
+    if len(fetcher)>0:
+        print fetcher
 
 
-## Replace the method argument with the string to be parsed
+
+## Replace the method argument with the string to be parsed, or you can modify the script to read the data from the text file 
 
 
-fetch_email_address("HELLO THERE SAPPAL.TUSHAR@GMAIl.COM from sappal.tushar@hotmail.com we will be meeting at around @2pm andl aslo @4pm")
+fetch_email_address("Replace this argument with the string to be parsed")
 
 
 
