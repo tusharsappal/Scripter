8dd53af563b8b7a27525a4d6a9de265a4ffb33b0
Tushar Sappal <sappal.tushar@gmail.com>
2014-01-30 18:04:34 +0530

    Adding some touch up

--- python_scripts/python_cook_book_receipes/functions/any_number_args_in_a_func.py
+++ python_scripts/python_cook_book_receipes/functions/any_number_args_in_a_func.py
@@ -1,7 +1,21 @@
 __author__ = 'tusharsappal'
 
+## Courtesy Python cook Book  7.1
+## This script accepts  any number of arguments and prints the average of the values
+
+
 
 def any_num_args(first, *rest):
-    avg = (first+sum(*rest))/(1+len(*rest))
+    avg = (first+sum(rest))/(1+len(rest))
     print avg
+
+
+
+## Example Call number 1
+any_num_args(1,2,3,4,5)
+
+## Example Call number 2
+any_num_args(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
+
+
 
