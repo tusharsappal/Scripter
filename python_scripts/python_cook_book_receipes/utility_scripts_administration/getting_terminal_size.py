a7ca12f4d18e19f2a3fcb851c2624dc40d6898d7
Tushar Sappal <sappal.tushar@gmail.com>
2014-02-04 16:00:59 +0530

    touch up

--- python_scripts/python_cook_book_receipes/utility_scripts_administration/getting_terminal_size.py
+++ python_scripts/python_cook_book_receipes/utility_scripts_administration/getting_terminal_size.py
@@ -4,8 +4,8 @@ __author__ = 'tusharsappal'
 ## This simple script tells you the size of the terminal
 ## Courtesy Python Cook Book
 
-
 import os
+
 
 def get_terminal_size():
     print os.get_terminal_size()
